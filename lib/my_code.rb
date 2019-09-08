a75f4a9bcb6d0adb0b96e43ef30de2f1c12342ac
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-09-08 20:00:04 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,77 @@
 # My Code here....
+def map_to_negativize(array) 
+  i = 0
+  while i < array.length do
+    array[i] = -(array[i])
+    i += 1
+  end
+  array
+end
+
+def map_to_no_change(array)
+  array
+end
+
+def map_to_double(array)
+  i = 0
+  while i < array.length do
+    array[i] = (array[i]) * 2
+    i += 1
+  end
+  array
+end
+
+def map_to_square(array)
+    i = 0
+  while i < array.length do
+    array[i] = (array[i]) * (array[i])
+    i += 1
+  end
+  array
+end
+
+def reduce_to_total(array, total = 0)
+    i = 0
+  while i < array.length do
+    total = total + array[i]
+    i += 1
+  end
+  total
+end
+
+#def reduce_to_all_true(array)
+ #   i = 0
+  #while i < array.length do
+   # !! (array[i])
+    
+    #i += 1
+  #end
+  #return true
+#end
+
+def reduce_to_all_true(array)
+   i = 0
+   while i < array.length do
+     if !!(array[i]) == false
+       return false
+     end
+     i += 1
+   end
+   return true
+end
+
+def reduce_to_any_true(array)
+   i = 0
+   while i < array.length do
+     if !!(array[i]) == true
+       return true
+     end
+     i += 1
+   end
+   return false
+end
+
+
+
+
+
