714314092c366d62b5131ef34ec46dd0d209f5cd
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-09-08 19:49:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,7 +54,11 @@ def reduce_to_all_true(array)
    i = 0
    while i < array.length do
      if !!(array[i]) == false
-       
+       return false
+     end
+     i += 1
+   end
+   return true
 end
 
 
