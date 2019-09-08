9afaa5c7f65126c4547e66cd2a28ada96e9504b9
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-09-08 19:29:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,35 @@
 # My Code here....
+def map_to_negativize(array) 
+  i = 0
+  while i < array.length do
+    array[i] = -(array[i])
+    i += 1
+  end
+  array
+end
+
+def map_to_no_change(array)
+  array
+end
+
+def map_to_double(array)
+  i = 0
+  while i < array.length do
+    array[i] = (array[i]) * 2
+    i += 1
+  end
+  array
+end
+
+def map_to_square(array)
+    i = 0
+  while i < array.length do
+    array[i] = (array[i]) * (array[i])
+    i += 1
+  end
+  array
+end
+
+def reduce_to_total(array)
+  
+end
